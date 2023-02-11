42f398188522358afb2032a009b9526a1598b783
SaanviPatnaik8711 <115956391+SaanviPatnaik8711@users.noreply.github.com>
2023-02-10 23:48:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function chgcolor(){
+    document.getElementById("Bckg").style.backgroundColor="blue";
+    document.getElementById("button").style.backgroundColor="pink";
+    document.getElementById("button").style.color="green";
+    document.getElementById("button").style.fontSize="20px";
+    document.getElementById("button").style.width="180px";
+}
